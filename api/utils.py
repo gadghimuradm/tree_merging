b9884f74da92716d5f460fb49f7449a9fc2a8f9a
gadghimuradm <gadghimuradm@gmail.com>
2024-02-19 20:55:47 +0300

    try to OOP

--- api/utils.py
+++ api/utils.py
@@ -1,102 +1,143 @@
 import difflib
 import re
-
+import json
+import xmltodict
+from xml.etree import ElementTree
 from dateutil import parser
+from abc import ABC, abstractmethod
 
 
-def _decode(o):
-    if isinstance(o, str) and o.isdigit():
-        return int(o)
-    elif isinstance(o, dict):
-        return {k: _decode(v) for k, v in o.items()}
-    elif isinstance(o, list):
-        return [_decode(v) for v in o]
-    else:
-        return o
+class Animal(ABC):
+
+    @abstractmethod
+    def speak(self):
+        pass
 
 
-def make_lists(json_data):
-    result = {}
-    for key, value in json_data.items():
-        key_name = key.split("[")[0]
+class TreeMerger(ABC):
+    tree = None
 
-        if key_name in result:
-            result[key_name].append(value)
-        else:
-            result[key_name] = [value]
-    return result
+    def __init__(self, request_body):
+        try:
+            self.merge_trees(self.parser(request_body))
+            self.normalize_data(self.tree)
+        except Exception:
+            self.tree = None
 
+    @abstractmethod
+    def parser(self, request_body) -> dict:
+        pass
 
-def merge_trees(data):
-    result = {}
-
-    for _, tree in data.items():
-        for key, value in tree.items():
-            key = key.split("[")[0]
-
-            if key in result.keys():
-                if isinstance(result[key], dict):
-                    result[key].update(value)
-                elif isinstance(result[key], list):
-                    result[key].append(value)
+    def make_lists(self, dictionary):
+        result = {}
+        for key, value in dictionary.items():
+            if key.endswith(']'):
+                key = key.split('[')[0]
+                if key in result.keys():
+                    if isinstance(result[key], dict):
+                        result[key].update(self.make_lists(value))
+                    elif isinstance(result[key], list):
+                        result[key].append(value)
+                    else:
+                        result[key] = [result[key], value]
                 else:
-                    result[key] = [result[key], value]
+                    result[key] = value
             else:
                 result[key] = value
+        return result
 
-    return result
+    def merge_trees(self, data):
+        result = {}
+
+        for _, tree in data.items():
+            for key, value in tree.items():
+                key = key.split('[')[0]
+                if key in result.keys():
+                    if isinstance(result[key], dict):
+                        result[key].update(self.make_lists(value))
+                    elif isinstance(result[key], list):
+                        result[key].append(value)
+                    else:
+                        result[key] = [result[key], value]
+                else:
+                    result[key] = value
+        self.tree = result
+
+    def get_tree(self):
+        return self.tree
+
+    def get_normalizers(self):
+        return {
+            'ДатаДокумента': self.normalize_date,
+            'СрокОплаты': self.normalize_time_period,
+        }
+
+    def normalize_data(self, data):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if key in self.get_normalizers():
+                    data[key] = self.get_normalizers()[key](value)
+                elif isinstance(value, dict):
+                    result = self.normalize_data(value)
+                    if result is not None:
+                        return result
+
+    @staticmethod
+    def normalize_date(date):
+        try:
+            parsed_date = parser.parse(date, dayfirst=True, fuzzy=True)
+            return parsed_date.strftime('%d.%m.%Y')
+        except ValueError:
+            return date
+
+    @staticmethod
+    def normalize_time_period(input_str):
+        result = {
+            "Г": 0,
+            "М": 0,
+            "Н": 0,
+            "Д": 0
+        }
+
+        matches = re.findall(r'(\d+)\s*(\w+)', input_str)
+
+        units = ["год", "месяц", "неделя", "день"]
+
+        for match in matches:
+            if not match[0].isdigit():
+                continue
+            value = int(match[0])
+            unit = match[1]
+
+            closest_unit = difflib.get_close_matches(unit, units, n=1, cutoff=0.5)
+            if closest_unit:
+                closest_unit = closest_unit[0]
+            else:
+                continue
+
+            index = units.index(closest_unit)
+
+            result[list(result.keys())[index]] += value
+
+        return f"{result['Г']}_{result['М']}_{result['Н']}_{result['Д']}"
 
 
-def normalize_date(date):
-    try:
-        parsed_date = parser.parse(date, dayfirst=True, fuzzy=True)
-        normalized_date = parsed_date.strftime('%d.%m.%Y')
-        return normalized_date
-    except ValueError:
-        return date
+class XMLTreeMerger(TreeMerger):
 
+    def parser(self, request_body) -> dict:
+        return json.loads(json.dumps(xmltodict.parse(request_body), indent=4), object_hook=self._decode)["root"]
 
-def normalize_time_period(input_str):
-    result = {
-        "Г": 0,
-        "М": 0,
-        "Н": 0,
-        "Д": 0
-    }
-
-    matches = re.findall(r'(\d+)\s*(\w+)', input_str)
-
-    units = ["год", "месяц", "неделя", "день"]
-
-    for match in matches:
-        if not match[0].isdigit():
-            continue
-        value = int(match[0])
-        unit = match[1]
-
-        closest_unit = difflib.get_close_matches(unit, units, n=1, cutoff=0.5)
-        if closest_unit:
-            closest_unit = closest_unit[0]
+    def _decode(self, o):
+        if isinstance(o, str) and o.isdigit():
+            return int(o)
+        elif isinstance(o, dict):
+            return {k: self._decode(v) for k, v in o.items()}
+        elif isinstance(o, list):
+            return [self._decode(v) for v in o]
         else:
-            continue
-
-        index = units.index(closest_unit)
-
-        result[list(result.keys())[index]] += value
-
-    return f"{result['Г']}_{result['М']}_{result['Н']}_{result['Д']}"
+            return o
 
 
-def normalize_data(data):
-    normalizers = {
-        'ДатаДокумента': normalize_date,
-        'СрокОплаты': normalize_time_period,
-    }
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if key in normalizers:
-                data[key] = normalizers[key](value)
-            elif isinstance(value, dict):
-                result = normalize_data(value)
-                if result is not None:
-                    return result
+class JSONTreeMerger(TreeMerger):
+    def parser(self, request_body) -> dict:
+        return json.loads(request_body)
--- api/views.py
+++ api/views.py
@@ -1,32 +1,20 @@
-import json
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from xml.etree import ElementTree
-import xmltodict
-from api.utils import _decode, merge_trees, normalize_data
+from api.utils import XMLTreeMerger, JSONTreeMerger
 
 
 class MergeTreesAPIView(APIView):
 
     def post(self, *args, **kwargs):
         if self.request.content_type == 'application/json':
-            try:
-                data = json.loads(self.request.body)
-            except ValueError:
-                return Response(status=status.HTTP_400_BAD_REQUEST)
+            data = JSONTreeMerger(self.request.body).get_tree()
         elif self.request.content_type == 'application/xml':
-            try:
-                data = json.loads(json.dumps(xmltodict.parse(self.request.body), indent=4), object_hook=_decode)["root"]
-            except ElementTree.ParseError:
-                return Response(status=status.HTTP_400_BAD_REQUEST)
+            data = XMLTreeMerger(self.request.body).get_tree()
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        try:
-            data = merge_trees(data)
-            normalize_data(data)
-        except Exception:
+        if not data:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
         return Response(data, status=status.HTTP_200_OK)
